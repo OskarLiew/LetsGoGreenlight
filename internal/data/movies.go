073758ba102e0ea5d9279b683fdb25868339415a
Oskar Liew <oskar@liew.se>
2025-12-04 22:32:49 +0100

    Validation

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,13 +1,34 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"github.com/OskarLiew/greenlight/internal/validator"
+)
 
 type Movie struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"-"` // When movie was added to DB
 	Title     string    `json:"title"`
-	Year      int       `json:"year,omitempty"`    // Release year
+	Year      int32     `json:"year,omitempty"`    // Release year
 	Runtime   Runtime   `json:"runtime,omitempty"` // Runtime in minutes
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int       `json:"version"` // Version number starts at 1 and increments each time movie info is updated
+}
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
 }
