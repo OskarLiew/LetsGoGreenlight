ef8062322d3a6ef46178a2c2a5affd7905081efb
Oskar Liew <oskar@liew.se>
2025-12-04 19:58:53 +0100

    Custom JSON marshalling

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -7,7 +7,7 @@ type Movie struct {
 	CreatedAt time.Time `json:"-"` // When movie was added to DB
 	Title     string    `json:"title"`
 	Year      int       `json:"year,omitempty"`    // Release year
-	Runtime   int32     `json:"runtime,omitempty"` // Runtime in minutes
+	Runtime   Runtime   `json:"runtime,omitempty"` // Runtime in minutes
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int       `json:"version"` // Version number starts at 1 and increments each time movie info is updated
 }
