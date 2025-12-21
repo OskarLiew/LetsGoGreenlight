b517f7cea5587f572c77472d204a4a4ffaf6d207
Oskar Liew <oskar@liew.se>
2025-12-21 13:16:51 +0100

    Set read permissions by default

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -53,6 +53,12 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	err = app.models.Permissions.AddForUser(user.ID, data.MoviesRead)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	token, err := app.models.Tokens.New(user.ID, 72*time.Hour, data.ScopeActivation)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
